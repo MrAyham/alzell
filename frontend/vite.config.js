32ae195b696310af72a4eb52153784a6a67135b8
MrAyham <h.b.k.ayhm@gmail.com>
2025-07-12 06:15:49 -0400

    fix: disable module preload polyfill

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   build: {
     target: 'esnext',
     minify: 'esbuild',
+    // Disable the module preload polyfill because it relies on
+    // `new Function` which is blocked in strict CSP environments
+    // like Vercel by default.
+    modulePreload: {
+      polyfill: false,
+    },
   },
   esbuild: {
     legalComments: 'none',
